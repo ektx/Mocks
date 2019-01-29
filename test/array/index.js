63633f5ccd5d6c0b7ad144fd8fba218abb84b2a6
ektx <530675800@qq.com>
2019-01-29 15:54:57 +0800

    优化示例与优化固定值的生成

--- test/array/index.js
+++ test/array/index.js
@@ -6,30 +6,23 @@ export default `
 option = {
     type: 'object',
     data: {
-        // 生成数组
-        array: {
+        arr: {
             type: 'array',
+            // 最小 2个元素
+            min: 2,
+            // 最多 10个元素
             max: 10,
-            min: 5,
             data: [{
                 type: 'string',
-                data: '1-10'
-            },{
-                type: 'number',
-                length: 10
-            },{
-                type: 'object',
-                data: {
-                    hello: {
-                        type: 'string',
-                        data: '1-10'
-                    }
-                }
+                // 最长 10 个字母
+                max: 10,
+                // 最少 2 个字母
+                min: 2
             }]
         },
         
         // 生成二维数组
-        array2: {
+        arrAir: {
             type: 'array',
             min: 10,
             data: [
@@ -38,17 +31,50 @@ option = {
                     min: 5,
                     data: [{
                         type: 'string',
-                        data: '1-10'
+                        max: 10
                     }]
                 }
             ]
         },
+
+        // 生成数组
+        pro: {
+            type: 'array',
+            max: 10,
+            min: 5,
+            data: [{
+                type: 'string',
+                max: 10
+            },{
+                type: 'number',
+                length: 10
+            },{
+                type: 'object',
+                data: {
+                    hello: {
+                        type: 'string',
+                        max: 10
+                    }
+                }
+            }]
+        },
         
-        // 生成指定内容
+        // 固定内容的生成
+        // 生成指定数组中的内容
         array3: {
             type: 'array',
             min: 5,
             data: ['ued', 'f2e']
+        },
+
+        // 随机生成 数组或是 'hello world'
+        array4: {
+            type: 'array',
+            max: 5,
+            data: [
+                ['ued', 'f2e'],
+                'hello world'
+            ]
         }
     }
 };
