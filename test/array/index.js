afa841277e00db2ea04830d9010a30a957bdeca1
ektx <530675800@qq.com>
2019-01-31 15:15:11 +0800

    更新示例

--- test/array/index.js
+++ test/array/index.js
@@ -13,96 +13,144 @@ option = {
             min: 2,
             // 最多 10个元素
             max: 10,
-            data: [{
-                type: 'string',
-                // 最长 10 个字母
-                max: 10,
-                // 最少 2 个字母
-                min: 2
-            }]
+            mark: '生成一个数字数组',
+            data: {
+                type: 'number',
+                max: 100,
+            }
         },
         
         // 生成二维数组
         arrAir: {
             type: 'array',
             min: 3,
-            data: [
-                {
-                    type: 'array',
-                    min: 5,
-                    data: [{
-                        type: 'string',
-                        max: 10
-                    }]
+            data: {
+                type: 'array',
+                min: 5,
+                data: {
+                    type: 'number',
+                    max: 100
                 }
-            ]
+            }
         },
 
-        // 生成数组
+        // 生成成绩单数组
         pro: {
             type: 'array',
             max: 4,
             min: 2,
-            data: [{
-                type: 'string',
-                max: 10
-            },{
-                type: 'number',
-                length: 10
-            },{
+            data: {
                 type: 'object',
                 data: {
-                    hello: {
+                    name: {
                         type: 'string',
-                        max: 10
+                        max: 2,
+                        data: 'CNBJX'
+                    },
+                    sex:  {
+                        type: 'string',
+                        data: ['男','女']
+                    },
+                    grade: {
+                        type: 'number',
+                        max: 100
                     }
                 }
-            }]
+            }
         },
         
         // 固定内容的生成
         // 生成指定数组中的内容
         array3: {
             type: 'array',
-            min: 5,
+            min: 3,
             data: ['ued', 'f2e']
         },
-
-        // 随机生成 数组或是 'hello world'
+        
+        // array
+        // 随机生成数组或是 'hello world'
         array4: {
             type: 'array',
-            max: 5,
+            min: 2,
             data: [
+                // 输出此数组
                 ['ued', 'f2e'],
+                // 或此字符串
                 'hello world'
             ]
         },
         
-        // Step 示例
+        // array
+        array5: {
+            type: 'array',
+            max: 5,
+            min: 1,
+            data: [
+                // 生成随机数字
+                {
+                    type: 'number',
+                    max: 100
+                },
+                // 直接输出
+                [{
+                    type: 'string',
+                    max: 5
+                }],
+                // 直接输出
+                [{
+                    name: 'ektx',
+                    age: 28,
+                    type: 'test'
+                }]
+            ]
+        },
+        
+        // Number - step+
         // 生成 2019之后的年份，最多生成5年
-        stepUp: {
+        increment: {
             type: 'array',
-            max: 5,
-            step: 1,
-            data: 2019
+            min: 3,
+            data: 2019,
+            step: 1
         },
         
+        // Number - step-
         // 生成到2019年的前几年，最多前5年
-        stepDown: {
+        decrement: {
+            type: 'array',
+            min: 3,
+            data: 2019,
+            step: -1
+        },
+        
+        // Number
+        repeatNum: {
             type: 'array',
             max: 5,
-            step: -1,
             data: 2019
         },
         
-        // 生成项目编号 
+        // String - step
+        // 在有 step 时，字符串会生成递增
+        // 如：生成Top5 
         stepStr: {
             type: 'array',
             min: 5,
             step: 1,
             data: 'TOP.'
+        },
+        
+        // String
+        // 对于字符串直接随机生成个数
+        str: {
+            type: 'array',
+            max: 5,
+            data: 'hello'
         }
     }
 };
+
+
+
 
 `
